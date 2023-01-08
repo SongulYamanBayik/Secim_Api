bfcd921866470741e2f26f8c38d1a2a32ac37e7b
Songül YAMAN BAYIK <snglymn@gmail.com>
2023-01-08 12:03:13 +0300

    ders 61

---
+++ Secim_Api/Models/ElectricChart.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Secim_Api.Models
+{
+    public class ElectricChart
+    {
+        public ElectricChart()
+        {
+            Counts = new List<int>();
+        }
+        public string ElectricDate { get; set; }
+        public List<int> Counts { get; set; }
+    }
+}
--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -1,20 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Secim_Api.Hubs;
-using Secim_Api.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Secim_Api
+namespace WebApplication1
 {
     public class Startup
     {
@@ -28,14 +23,7 @@ namespace Secim_Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<Context>(options =>
-            {
-                options.UseSqlServer(Configuration["ConStr"]);
-            });
-            services.AddScoped<ElectricService>();
-            services.AddCors();
-            services.AddSignalR();
-            services.AddControllers();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,22 +33,24 @@ namespace Secim_Api
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
-            app.UseCors(x => x
-            .AllowCredentials()
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .SetIsOriginAllowed(orign => true));
 
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-                endpoints.MapHub<ElectricHub>("/ElectricHub");
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
